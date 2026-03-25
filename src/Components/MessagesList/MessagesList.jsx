1b2e5d59524f8b54d9483e876175e4887f51f92d
tadeo.dev <tadeo.otaola@gmail.com>
2026-03-25 15:04:58 -0300

    mejora manejo de mensajes y scroll automático en el chat

--- src/Components/MessagesList/MessagesList.jsx
+++ src/Components/MessagesList/MessagesList.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Message from "../Message/Message";
 import './MessagesList.css';
+import { useRef } from "react"
+import { useEffect } from "react"
 
 const MessagesList = ({ messages, deleteMessageById }) => {
+
+  const contenedorRef = useRef(null)
+
+  useEffect(() => {
+  if (contenedorRef.current) {
+    contenedorRef.current.scrollTop = contenedorRef.current.scrollHeight
+  }
+  }, [messages])
+
   return (
-    <div className="contenedor-mensajes">
+    <div className="contenedor-mensajes" ref={contenedorRef}>
       {messages.length === 0 ? (
         <span className="sin-mensajes">Aún no tienes mensajes</span>
       ) : (
